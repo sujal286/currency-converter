cebf8be32c3b4282f54e422b29afcd8d39e5cc9c
Sujal Saha <sujalsaha14@gmail.com>
2024-06-23 21:45:50 +0530

    add api

---
+++ script.js
@@ -0,0 +1,21 @@
+const BASE_URL ="https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+
+const containers = document.querySelectorAll(".container select");
+
+let i=0;
+for(let select of containers){
+    for(currCode in countryList){
+        let newOption=document.createElement("option");
+        newOption.innerText=currCode;
+        newOption.value=currCode;
+        if(select.name === "from" && currCode === "USD")
+            {
+                newOption.selected="selected";
+            }
+        else if(select.name === "to" && currCode === "INR")
+            {
+                newOption.selected="selected";
+            }
+        select.append(newOption);
+    }
+}
